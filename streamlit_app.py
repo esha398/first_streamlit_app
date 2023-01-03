7433a3f45526300ff25a0bb16abe44c403c4abd9
esha398 <121863048+esha398@users.noreply.github.com>
2023-01-03 11:03:54 +0000

    Update streamlit_app.py
    Added build you own smoothie option

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text("🍛Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥬Kale, Spinach and Rocket Smoothie")
 streamlit.text("🥚🍳Hard-Boiled Free renage Eggs")
 streamlit.text("🥑🥪Avocado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
